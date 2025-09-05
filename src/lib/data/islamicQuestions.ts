7b48dca8ad8a7613c4f6bdbca199fa3941a599ce
yayagee0 <ghassanahmed@hotmail.com>
2025-09-05 16:43:29 +0400

    Create islamicQuestions.ts to aggregate question sets

---
+++ src/lib/data/islamicQuestions.ts
@@ -0,0 +1,17 @@
+import { allahQuestions } from "./allah";
+import { prophetQuestions } from "./prophet";
+import { quranQuestions } from "./quran";
+import { prayerQuestions } from "./prayer";
+import { lifeDeathQuestions } from "./lifeDeath";
+import { akhlaqQuestions } from "./akhlaq";
+import { identityQuestions } from "./identity";
+
+export const islamicQuestions = [
+  ...allahQuestions,
+  ...prophetQuestions,
+  ...quranQuestions,
+  ...prayerQuestions,
+  ...lifeDeathQuestions,
+  ...akhlaqQuestions,
+  ...identityQuestions
+];
